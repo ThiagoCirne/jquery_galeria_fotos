ee23d20f49138d6e0bc7f93cfd0c6683c2297668
Thiago Cirne <digimagemcftv@gmail.com>
2023-12-21 20:37:36 -0300

    comit inicial

---
+++ main.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    $('header button').click(function() {
+        $('form').slideDown();     /*efeito para o form abrir suave para baixo*/  
+    })
+
+    $('#botao-cancelar').click(function() {
+        $('form').slideUp();
+    })
+
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
+        const novoItem = $('<li style="display: none"></li>');/*foi colocado display none por conta do fadeIn*/
+        $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem);
+        $(`
+            <div class="overlay-imagem-link">
+                <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
+                    Ver imagem em tamanho real
+                </a>
+            </div>
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(1000); /* inserindo a imagem com efeito em 1s*/
+        $('#endereco-imagem-nova').val('');
+        
+    })
+})
+
